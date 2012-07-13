bbb9e1f791a346d79431d90a5b3d4b58f3ecfd84
Ezekiel Kigbo <theatlasroom@gmail.com>
2012-07-13 15:47:56 +1000

    starting to refactor, will create seperate files for statemanagement and base states

--- stateify.js
+++ stateify.js
@@ -10,88 +10,91 @@
  */
 //define the clasess
 var uiclass = {
-	inactive: ".uia-inactive",
-	selected: ".uia-selected",
-	hidden: ".uia-hidden"	
+	"inactive": ".uia-inactive",
+	"selected": ".uia-selected",
+	"hidden": ".uia-hidden"	
 }
 //define the states
 var uistate = {
-	inactive: "uia-inactive",
-	selected: "uia-selected",
-	hidden: "uia-hidden"
+	"inactive": "uia-inactive",
+	"selected": "uia-selected",
+	"hidden": "uia-hidden"
 }
 
 function ActivateIfSelected(elem, target){
-	if (HasSelected(elem))
+	if (HasSelected(elem)){
+		console.log("activating");
 		Activate(target);
-	else 		
-		Deactivate(target);	
+	}
+	else {
+		console.log("deactivating");
+		Deactivate(target);
+	}	
 }
 
-function Activate(elem){if (NotActive(elem)) $(elem).removeClass(uistate.inactive);}
+function Activate(elem){if (NotActive(elem)) $(elem).removeClass(uistate['inactive']);}
 function Deactivate(elem){
 	if (!NotActive(elem)){ 
-		$(elem).addClass(uistate.inactive);
+		$(elem).addClass(uistate['inactive']);
 		//unselect any children that are selected
-		$($(elem).find(uiclass.selected)).each(function(){ToggleState($(this), uistate.selected);});			
+		$($(elem).find(uiclass['selected'])).each(function(){ToggleState($(this), 'selected');});			
 	}
 }
 
 function IsSelected(elem){
 	//return true if the item is the selected state	
-	return InState(elem, uistate.selected);		
+	return InState(elem, 'selected');		
 }
 
 function IsInactive(elem){
 	//return true if the item is inactive	
-	return InState(elem, uistate.inactive);
+	return InState(elem, 'inactive');
 }
 
 function IsHidden(elem){
 	//return true if the item is inactive	
-	return InState(elem, uistate.hidden);	
+	return InState(elem, 'hidden');	
 }
 
 function HasHidden(elem){
-	//console.log($(elem).find(uiclass.selected));
-	return HasChildInState(elem, uiclass.hidden);	
+	//console.log($(elem).find(uiclass['selected']));
+	return HasChildInState(elem, 'hidden');	
 }
 
 function HasInactive(elem){
-	//console.log($(elem).find(uiclass.selected));
-	return HasChildInState(elem, uiclass.inactive);	
+	//console.log($(elem).find(uiclass['selected']));
+	return HasChildInState(elem,'inactive');	
 }
 
-
 function HasSelected(elem){
-	//console.log($(elem).find(uiclass.selected));
-	return HasChildInState(elem, uiclass.selected);
+	//console.log($(elem).find(uiclass['selected']));
+	return HasChildInState(elem, 'selected');
 }
 
 function NotActive(elem){
 	//checks if this element or its parents are set to inactive
-	var inactive = $(elem).parents(uiclass.inactive);
-	if ($(inactive).length > 0 || $(elem).hasClass(uistate.inactive))
+	var inactive = $(elem).parents(uiclass['inactive']);
+	if ($(inactive).length > 0 || $(elem).hasClass(uistate['inactive']))
 		return true;
 	return false;
 }
 
 function RemoveSiblingState(elem, state){
-	var sib_selector = "."+state;
+	var sib_selector = uiclass[state];
 	//iterate over the siblings of this element and remove the uia class from them
-	$(elem).siblings(sib_selector).removeClass(state);
+	$(elem).siblings(sib_selector).removeClass(uistate[state]);
 }
 
 
 function InState(elem, state){
 	//return true if the item is in the state specified	
-	if ($(elem).hasClass(state))
+	if ($(elem).hasClass(uistate[state]))
 		return true;
 	return false;		
 }
 
 function HasChildInState(elem, state){
-	if ($(elem).find(state).length > 0)
+	if ($(elem).find(uiclass[state]).length > 0)
 		return true;
 	return false;
 }
@@ -99,18 +102,18 @@ function HasChildInState(elem, state){
 function ToggleState(elem, state){
 	//toggles the state of an element, checks neighbouring to turn them to the off state
 	//this assumes only one object can be in a particular state (within a grouping)
-	if ($(elem).hasClass(state))
-		$(elem).removeClass(state);
+	if ($(elem).hasClass(uistate[state]))
+		$(elem).removeClass(uistate[state]);
 	else 
-		$(elem).addClass(state);
+		$(elem).addClass(uistate[state]);
 }
 
 function ToggleSelectedState(elem){
 	//check that it is not in the inactive state (ignore the toggle command if it is)
 	if (NotActive(elem)) return;
-	RemoveSiblingState(elem, uistate.selected);	
-	ToggleState(elem, uistate.selected);
+	RemoveSiblingState(elem, 'selected');	
+	ToggleState(elem, 'selected');
 } 
 
-function ToggleInactiveState(elem){ToggleState(elem, uistate.inactive);}
-function ToggleHiddenState(elem){ToggleState(elem, uistate.hidden);} 
+function ToggleInactiveState(elem){ToggleState(elem, 'inactive');}
+function ToggleHiddenState(elem){ToggleState(elem, 'hidden');} 
